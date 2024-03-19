0f848f81c44574fee30e718bf4e15f51056f7358
Jeff <jeparsons@teksystems.com>
2024-03-19 13:59:57 -0400

    sample test class

---
+++ src/test/java/TestClazz.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestClazz {
+
+    @Test
+    void simpleExampleTest() {
+        Assertions.assertTrue(true);
+        /**
+         * TODO
+         write test code here if desired
+         */
+    }
+}
