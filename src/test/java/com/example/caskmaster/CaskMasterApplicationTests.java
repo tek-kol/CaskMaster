2d6a85b673cbd1b7702b9356eb9ae2a91999b764
jkol <jkol@teksystems.com>
2024-03-19 11:18:57 -0500

    Switch to Maven

--- src/test/java/com/example/caskmaster/CaskMasterApplicationTests.java
+++ src/test/java/com/example/caskmaster/CaskMasterApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CaskMasterApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
