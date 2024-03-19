a913b167b983e8c6a6f8f51b818a6445fae68ac9
jkol <jkol@teksystems.com>
2024-03-19 14:57:23 -0500

    Added test: RestTemplate

---
+++ src/main/java/com/example/caskmaster/dto/AddressDto.java
@@ -0,0 +1,14 @@
+package com.example.caskmaster.dto;
+
+public class AddressDto {
+    private String address1;
+    private String address2;
+    private String address3;
+    private String city;
+    private String stateProvince;
+    private String postalCode;
+    private String country;
+    private String state;
+    private String street;
+
+}
--- src/test/java/TestClazz.java
+++ src/test/java/TestClazz.java
@@ -1,14 +1,20 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 public class TestClazz {
 
     @Test
     void simpleExampleTest() {
-        Assertions.assertTrue(true);
-        /**
-         * TODO
-         write test code here if desired
-         */
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl = "https://api.openbrewerydb.org/v1/breweries";
+        ResponseEntity<String> response
+                = restTemplate.getForEntity(fooResourceUrl + "/1", String.class);
+        Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
+
+
+
     }
 }
