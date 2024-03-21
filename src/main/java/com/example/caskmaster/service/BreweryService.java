24123d79c14bfe1990f7c614be874c88bae54723
Jeff <jeparsons@teksystems.com>
2024-03-21 15:50:50 -0400

    Assignment 2

--- src/main/java/com/example/caskmaster/service/BreweryService.java
+++ src/main/java/com/example/caskmaster/service/BreweryService.java
@@ -1,22 +1,29 @@
 package com.example.caskmaster.service;
 
-import org.springframework.http.ResponseEntity;
+import com.example.caskmaster.dto.Brewery;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @Service
 public class BreweryService {
-//RestTemplate restTemplate = new RestTemplate();
-private static final String API_URL= "https://api.openbrewerydb.org/v1";
+    //RestTemplate restTemplate = new RestTemplate();
+    private static final String API_URL = "https://api.openbrewerydb.org/v1";
 
 
-
-    public String getAllBreweries(){
+    public String getAllBreweries() {
         // configure rest template
         RestTemplate restTemplate = new RestTemplate();
         // call brewery api
         // return results
+
         return restTemplate.getForEntity(API_URL + "/breweries", String.class).getBody();
     }
 
+
+    public List<Brewery> getBreweries() {
+        // FIXME: implement this and remove previous method
+        return null;
+    }
 }
