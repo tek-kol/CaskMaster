0fecbaeb40615129a201469b7afc06df568f6299
Jeff <jeparsons@teksystems.com>
2024-04-09 13:41:53 -0400

    added Exception class to support describing what validation failed

---
+++ src/main/java/com/example/caskmaster/exception/DataValidationException.java
@@ -0,0 +1,21 @@
+package com.example.caskmaster.exception;
+
+public class DataValidationException extends RuntimeException {
+
+    public DataValidationException() {
+
+    }
+
+    public DataValidationException(String message) {
+        super(message);
+    }
+
+    public DataValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataValidationException(Throwable cause) {
+        super(cause);
+    }
+
+}
